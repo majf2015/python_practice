59d1a170170e08a75280472b38744ee093c5d6e4
马金凤 <1021008546@qq.com>
2015-06-08 21:06:07 +0800

    更新回文函数

--- list_function.py
+++ list_function.py
@@ -1,7 +1,9 @@
+#coding = utf-8
 import time
 import math
 
 
+#---查找数列中最大的元素---
 def max_element(l):
     max_element = 0
     for e in l:
@@ -10,34 +12,42 @@ def max_element(l):
     print  max_element
 
 
+#---反转数列---
 def reverse_list(l):
     print l[::-1]
 
 
+#---查找指定的元素是否在数列中---
 def find_element(l, elem):
     for e in l:
         if e == elem:
             print 'l[%s] = %s' % (l.index(e), e)
 
 
+#---回文---
 def palindrome_string(s):
-    for e in s:
-        if e != s[-s.index(e) - 1]:
-            print "this string is not a palindrome"
-            break
-        elif s.index(e) >= len(s) / 2 - 1 and e == s[-s.index(e) - 1]:
-            print "this string is a palindrome"
-            break
+    if len(s) == 0:
+        print "this string is empty"
+    else:
+        for e in s:
+            if e != s[-s.index(e) - 1]:
+                print "this string is not a palindrome"
+                break
+            elif s.index(e) >= len(s) / 2 - 1 and e == s[-s.index(e) - 1]:
+                print "this string is a palindrome"
+                break
 
 
+#---查找数列中前20位完全平方数---
 def square(l):
     li = []
     for e in l:
-        if int(math.sqrt(e)) * int(math.sqrt(e)) == e:
+        if int(math.sqrt(e)) * int(math.sqrt(e)) == e and len(li) < 20:
             li.append(e)
     print li
 
 
+#---for循环求数列中元素的和---
 def for_loop(l):
     sum = 0
     for e in l:
@@ -45,6 +55,7 @@ def for_loop(l):
     print 'sum = %d' % sum
 
 
+#---while循环求数列中元素的和---
 def while_loop(l):
     sum = 0
     n = 0
@@ -54,6 +65,7 @@ def while_loop(l):
     print 'sum = %d' % sum
 
 
+#---递归循环求数列中元素的和---
 def recursive_loop(l):
     if len(l) == 0:
         return 0
@@ -62,10 +74,10 @@ def recursive_loop(l):
         return recursive_loop(l) + p
 
 
-
+#“主运行程序，计算运行时间”
 start_time = time.clock()
 list = [1, 2, 3, 8, 10, 8, 9, 6, 4, 16]
-string = 'ab0120ba'
+string = ''
 
 max_element(list)
 reverse_list(list)
