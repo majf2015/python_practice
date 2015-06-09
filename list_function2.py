459efc97795e4f4028a2a53b82cedfee470b99c3
马金凤 <1021008546@qq.com>
2015-06-09 18:47:41 +0800

    增加备注

--- list_function2.py
+++ list_function2.py
@@ -1,10 +1,13 @@
+# encoding:utf-8
 import math
 
 
+#---连接---
 def connect(l1, l2):
     print l1 + l2
 
 
+#---斐波那契---
 def fibonacci(a, b):
     f = [a, b]
     for i in range(2, 10):
@@ -13,6 +16,7 @@ def fibonacci(a, b):
     print f
 
 
+#---获取数字---
 def figure(num):
     d = []
     for e in num:
@@ -20,8 +24,23 @@ def figure(num):
             d.append(e)
     print d
 
+
+#---交替合并---
+def interchange_combine(l1, l2):
+    n = 1
+    for e in l2:
+        l1.insert(n, e)
+        n += 2
+    print l1
+
+
+
+
+#---主运行程序---
 list = [1, 2, 4, 16]
-list2 = [6, 8, 9, 11]
+list2 = [6, 8, 9, 11,12]
+
+interchange_combine(list, list2)
 connect(list, list2)
 fibonacci(1, 2)
 num = 'and456'
