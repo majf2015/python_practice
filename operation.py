c3f397300352ecf5924b59d2bc5a475aec320f3c
马金凤 <1021008546@qq.com>
2015-06-11 11:22:45 +0800

    优化数列表示的加法，能处理整数相加，小数与整数相加及小数数相加，小数与整数相加

--- operation.py
+++ operation.py
@@ -4,8 +4,20 @@
 #---用数列表示加运算---
 def add(s1, s2):
     ss = ['0', '0']
-    ss1 = s1.split('.')
-    ss2 = s2.split('.')
+    if int(float(s1)) == float(s1):
+        if int(float(s2)) == float(s2):
+            ss1 = [str(s1), '0']
+            ss2 = [str(s2), '0']
+        else:
+            ss1 = [str(s1), '0']
+            ss2 = s2.split('.')
+    else:
+        if int(float(s2)) == float(s2):
+            ss1 = s1.split('.')
+            ss2 = [str(s2), '0']
+        else:
+            ss1 = s1.split('.')
+            ss2 = s2.split('.')
     sss = str(float('0.' + ss1[1]) + float('0.' + ss2[1]))
     ss[0] = str(int(ss1[0]) + int(ss2[0]) + int(sss.split('.')[0]))
     ss[1] = str(int(sss.split('.')[1]))
@@ -41,13 +53,14 @@ def reduce(s1, s2):
     print ss
 
 
-#---用数列表示乘法运算---
-
-
-
-
 
 s1 = '33.33'
-s2 = '232.776'
+s2 = '232.276'
+s3 = '33'
+s4 = '232'
+
 add(s1, s2)
+add(s1, s4)
+add(s2, s3)
+add(s3, s4)
 reduce(s1, s2)
