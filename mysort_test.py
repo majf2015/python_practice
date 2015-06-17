9fae340fb966958c23075b241f102a0aa1684d49
马金凤 <1021008546@qq.com>
2015-06-17 21:15:52 +0800

    增加归并排序单元测试方法：

--- mysort_test.py
+++ mysort_test.py
@@ -5,6 +5,7 @@ l = [[1, 5, 36, 67, 4, 7, 78, 100], [111, 23, 111], [1]]
 result_sort = [[1, 4, 5, 7, 36, 67, 78, 100], [23, 111, 111], [1]]
 result_my_reverse = [[100, 78, 7, 4, 67, 36, 5, 1], [111, 23, 111], [1]]
 result_insert_sort = [[1, 4, 5, 7, 36, 67, 78, 100], [23, 111, 111], [1]]
+result_add_up_sort = [[1, 4, 5, 7, 36, 67, 78, 100], [23, 111, 111], [1]]
 for i in range(len(l)):
     try:
         ret_sort = mysort.select_sort(l[i])
@@ -26,7 +27,6 @@ for i in range(len(l)):
     except:
         print "my_reverse exception", i
 
-
     try:
         ret_insert_sort = mysort.insert_sort(l[i])
         if ret_insert_sort == result_insert_sort[i]:
@@ -36,6 +36,18 @@ for i in range(len(l)):
                   (i, ret_insert_sort, result_insert_sort[i])
     except:
         print "insert sort exception", i
+
+    try:
+        ret_add_up_sort = mysort.add_up_sort(l[i])
+        print ret_add_up_sort
+        if ret_add_up_sort == result_add_up_sort[i]:
+            print 'success', ret_add_up_sort
+        else:
+            print 'error3 : The %sth sorted %s is not equal to the expected result %s' % \
+                  (i, ret_add_up_sort, result_add_up_sort[i])
+    except:
+        print "add_up sort exception", i
+
 
 
 
