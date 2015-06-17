03ffce5bb32de5c548e594d8f3c6caeef2e3a721
马金凤 <1021008546@qq.com>
2015-06-17 21:14:39 +0800

    增加归并排序函数

--- mysort.py
+++ mysort.py
@@ -45,5 +45,37 @@ def insert_sort(l):
     return my_sorted
 
 
+def tear_list(sorted1, sorted2):
+    ret_sort = []
+    i = j = 0
+    while i < len(sorted1) and j < len(sorted2):
+        if sorted1[i] < sorted2[j]:
+            ret_sort.append(sorted1[i])
+            i += 1
+        else:
+            ret_sort.append(sorted2[j])
+            j += 1
+    while i < len(sorted1):
+        ret_sort.append(sorted1[i])
+        i += 1
+    while j < len(sorted2):
+        ret_sort.append(sorted1[j])
+        j += 1
+
+
+
+def add_up_sort(l):
+    if len(l) == 0:
+        raise  999
+    elif len(l) == 1:
+        return l
+    else:
+        sorted1 = add_up_sort(l[: len(l) / 2])
+        sorted2 = add_up_sort(l[len(l) / 2 :])
+    return tear_list(sorted1, sorted2)
+
+
+l = [1, 5, 36, 67, 4, 7, 78, 100]
+add_up_sort(l)
 
 
