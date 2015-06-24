c0b596fd5f26f9b95c8760f8cf9ce5858476eac7
马金凤 <1021008546@qq.com>
2015-06-24 20:45:10 +0800

    增加快速排序

--- quick_sort.py
+++ quick_sort.py
@@ -7,13 +7,25 @@ def quick_sort(l):
         low_list = []
         high_list = []
         middle_e = l[0]
-        for i in range(len(l)):
+        for i in l:
             if i < middle_e:
                 low_list.append(i)
             else:
                 high_list.append(i)
-        return add_list(quick_sort(low_list), quick_sort(high_list))
+        low_list = quick_sort(low_list)
+        high_list = quick_sort(high_list)
+
+    return add_list(low_list, high_list)
 
 
 def add_list(l1, l2):
-    pass
+    re_sorted = []
+    if len(l1) != 0:
+        for i in l1:
+            re_sorted.append(i)
+    if len(l2) != 0:
+        for j in l2:
+            re_sorted.append(j)
+    return re_sorted
+
+
